18d40344f4d7c4e24b8d36a3b08a49550d101c4c
Gobind Singh <gobindbhari@gmail.com>
2024-12-10 22:57:57 +0530

    set navbar

--- client/src/components/header/Navbar.jsx
+++ client/src/components/header/Navbar.jsx
@@ -19,11 +19,8 @@ const Navbar = () => {
                 setNavcon(true);
             }
         };
-
-        // Attach the scroll event listener
         window.addEventListener('scroll', handleScroll);
 
-        // Clean up the event listener on component unmount
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
@@ -60,7 +57,7 @@ const Navbar = () => {
                         {/* second */}
                         {navcon ?
                             (<div className='flex flex-col justify-center h-full  pt-2 lg:visible'>
-                                <div className='flex justify-center mb-3 gap-3 text-lg max-lg:invisible '>
+                                <div className='flex justify-center mb-3 gap-3 text-lg max-lg:hidden '>
                                     <div>
                                         <button className='w-16 py-1'>Stays</button>
                                     </div>
@@ -72,7 +69,7 @@ const Navbar = () => {
 
                             </div>)
                             : (
-                                <div className="outline outline-2 outline-slate-200  rounded-full ml-32">
+                                <div className="outline outline-2 outline-slate-200  rounded-full ml-20 max-lg:hidden">
                                     <div className="w-96 h-12 flex gap-3 px-2 justify-between items-center">
                                         <div className=""><button>Anywhere</button></div>
                                         <div className="border-r-2 h-[80%] w-1 my-auto"></div>
@@ -90,9 +87,9 @@ const Navbar = () => {
 
                         {/* third */}
                         <div className='flex gap-4 justify-center align-middle h-full'>
-                            <div className='flex gap-3 justify-center '>
+                            <div className='flex gap-2 justify-center '>
                                 <h3 className='m-auto hover:bg-slate-100 rounded-3xl px-3 py-2'>Airbnb your home </h3>
-                                <button className='hover:bg-slate-100 rounded-full px-4 '><img className='h-4 m-auto' src="images/navbar/earth.svg" alt="" /></button>
+                                <button className='hover:bg-slate-100 rounded-full px-2'><img className='h-4 m-auto' src="images/navbar/earth.svg" alt="" /></button>
                             </div>
                             <button onClick={() => {setIsOpen(isOpen ? false : true), setSignShow(false)}} className=' flex gap-2 rounded-full px-4 py-2 h-12 outline outline-slate-300 outline-1 hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
                                 <img className='h-4 m-auto' src="images/navbar/threelines.svg" alt="" />
@@ -143,24 +140,25 @@ const Navbar = () => {
 
 
                     {/* second Down */}
-                    {navcon ? (
-                        <div className="w-[67vw] mx-auto">
+                        <div className={navcon ? 'visible': 'hidden' }>
+                        <div className="w-[67vw] mx-auto max-lg:hidden ">
                             <div className="max-lg:invisible  top-20 -left-[25vw] outline outline-1 outline-slate-200 rounded-full shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] font-medium">
                                 <div className='  w-[67vw] flex items-center h-[70px]'>
-                                    <div onMouseEnter={() => setFirst(true)} onMouseLeave={() => setFirst(false)} className='flex  h-full w-fit hover:bg-slate-300 rounded-full pl-10 py-2'>
+                                
+                                    <div onMouseEnter={() => setFirst(true)} onMouseLeave={() => setFirst(false)} className='flex  h-full w-[40%] hover:bg-slate-300 rounded-full pl-10 py-2'>
                                         <div className="">
                                             <div>Where</div>
                                             <input className='h-4 w-52 outline-none font-normal bg-transparent' type="text" placeholder='Search destinatios' />
                                         </div>
-                                        <div style={hoverEffect} className="border-r-2 h-[80%] w-1  ml-28 my-auto"></div>
+                                        <div style={hoverEffect} className="border-r-2 h-[80%] w-1 my-auto lg:hidden"></div>
                                     </div>
 
-                                    <div onMouseEnter={() => { setFirst(true); setSecond(true) }} onMouseLeave={() => { setFirst(false); setSecond(false) }} className='mr-3 flex pl-10 py-3 hover:bg-slate-300 rounded-full h-full'>
+                                    <div onMouseEnter={() => { setFirst(true); setSecond(true) }} onMouseLeave={() => { setFirst(false); setSecond(false) }} className=' flex pl-10 py-3 hover:bg-slate-300 rounded-full h-full'>
                                         <div>
                                             <div>Chech in</div>
                                             <input className='outline-none font-normal h-4 w-28 bg-transparent' placeholder='Add dates' type="text" />
                                         </div>
-                                        <div style={hoverSecond} className="border-r-2 h-[80%] w-1 my-auto"></div>
+                                        <div style={hoverSecond} className="border-r-2 h-[80%] w-1 my-auto lg:hidden"></div>
                                     </div>
 
                                     <div onMouseEnter={() => { setThrid(true); setSecond(true) }} onMouseLeave={() => { setThrid(false); setSecond(false) }} className='flex rounded-full pl-10 py-3 h-full hover:bg-slate-300'>
@@ -168,7 +166,7 @@ const Navbar = () => {
                                             <div>Chech out</div>
                                             <input className='outline-none font-normal h-4 w-20 bg-transparent' placeholder='Add dates' type="text" />
                                         </div>
-                                        <div style={hoverThrid} className="border-r-2 h-[80%] w-1 my-auto"></div>
+                                        <div style={hoverThrid} className="border-r-2 h-[80%] w-1 my-auto lg:hidden"></div>
                                     </div>
 
                                     <div onMouseEnter={() => setThrid(true)} onMouseLeave={() => setThrid(false)} className='flex gap-4 pl-10 py-2 h-full hover:bg-slate-300 rounded-full w-full'>
@@ -182,7 +180,8 @@ const Navbar = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>) : ''}
+                        </div>
+                        </div>
                 </nav>
                 <NavDownBox />
             </header>
